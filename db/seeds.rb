d7338792030e9cbdc90b2a1cc198b3e58707733d
Dumitru <dgorgan3@gmail.com>
2015-07-15 04:09:36 +0000

    created new soviet seed data for experiment and cleaned up soviet listings with bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+soviets = Soviet.create([{name: 'Anatoly Anatolivich', rank: 'Private', position: 'Infantry', experience: 2, about: 'I vill crush you!'}])
